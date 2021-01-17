36c69ac3cc7832b3ed56630fd475c0314fd993f4
yozdas <bilgi[at]yusufozdas.com>
2021-01-18 00:41:24 +0300

    -if statement düzeltme -Try Catch blokları ekleme

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -4,67 +4,83 @@ namespace ClassMethodDemo
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            
-            
+
+
             char q;
 
             string No;
 
             Musteri musteri1 = new Musteri() { };
             MusteriManager musteriManager = new MusteriManager();
-
-            while (true)
+            try
             {
-                Console.WriteLine(" -Müşteri Eklemek için 1 \n -Silmek için 2 \n -Listelemek 3 girin ve Enter \n -Çıkmak için q");
-                q = Convert.ToChar(Console.ReadLine());
 
-                if (q == '1')
+
+
+                while (true)
                 {
-                    
-                                      
-                    Musteri musteri = new Musteri() { };
-                    Console.WriteLine("Müşteri Adı: ");
-                    musteri.Ad = Console.ReadLine().ToString();
+                    Console.WriteLine(" -Müşteri Eklemek için 1 \n -Silmek için 2 \n -Listelemek 3 girin ve Enter \n -Çıkmak için q");
+                    q = Convert.ToChar(Console.ReadLine());
 
-                    Console.WriteLine("Müşteri Soyadı: ");
-                    musteri.Soyad = Console.ReadLine().ToString();
+                    if (q == '1')
+                    {
 
-                    musteri.Id = rnd.Next(5000);
-                    musteri.MusteriNo = rnd.Next(50001, 100001).ToString();
 
-                    Musteri[] musteriler = new Musteri[] { musteri };
-                   
-                    musteriManager.Ekle(musteriler);
-                    musteriManager.Liste();
+                        Musteri musteri = new Musteri() { };
+                        Console.WriteLine("Müşteri Adı: ");
+                        musteri.Ad = Console.ReadLine().ToString();
 
-                        
-                }
-                else if (q == '2')
-                {
-                    Console.WriteLine("Silinecek Müşteri No:");
-                    No = Console.ReadLine().ToString();
-                    musteriManager.Sil(No);
-                    
+                        Console.WriteLine("Müşteri Soyadı: ");
+                        musteri.Soyad = Console.ReadLine().ToString();
 
-                }
-                else if (q == '3')
-                {
-                    Console.WriteLine("Müşteri Listesi");
-                    musteriManager.Liste();
-                }
-                if (q == 'q')
-                {
-                    break;
+                        musteri.Id = rnd.Next(5000);
+                        musteri.MusteriNo = rnd.Next(50001, 100001).ToString();
+
+                        Musteri[] musteriler = new Musteri[] { musteri };
+
+                        musteriManager.Ekle(musteriler);
+                        musteriManager.Liste();
+
+
+                    }
+                    else if (q == '2')
+                    {
+                        Console.WriteLine("Silinecek Müşteri No:");
+                        No = Console.ReadLine().ToString();
+                        musteriManager.Sil(No);
+
+
+                    }
+                    else if (q == '3')
+                    {
+                        Console.WriteLine("Müşteri Listesi");
+                        musteriManager.Liste();
+                    }
+                    else if (q == 'q')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("--------");
+                        Console.WriteLine("Hatalı Tuşlama...");
+                        Console.WriteLine("--------");
+                    }
                 }
             }
-            
-           
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Hatalı işlem: {ex.Message}");
+            }
+
+
         }
 
-       
+
     }
 }
